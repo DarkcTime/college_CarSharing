abdb6ff99e4f7ec07b7bf92b82d9c6a8c476ddbc
Vlad <vlad_chekmarev_01@mail.ru>
2020-08-27 10:35:22 +0300

    added stucture project

---
+++ CarSharing/Controller/SharedClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace CarSharing.Controller
+{
+    class SharedClass
+    {
+        public static void MessageBoxError(string message, string title = "Ошибка")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MessageBoxError(Exception ex, string title = "Ошибка")
+        {
+            MessageBox.Show(ex.Message, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MessageBoxWarning(string message, string title = "Проверьте введенные данные")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+        public static void MessageBoxInformation(string message, string title = "Успешно")
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public static void OpenNewPage(System.Windows.DependencyObject obj, Page page)
+        {
+            NavigationService service = NavigationService.GetNavigationService(obj);
+            service.Navigate(page);
+        }
+
+
+
+
+    }
+}
